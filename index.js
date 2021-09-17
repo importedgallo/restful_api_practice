087c75d87bbd072b09ebd758777483b8e4b51605
importedGallo <andre.mongard@gmail.com>
2021-09-17 18:26:30 +1000

    changes to index.js to get the api working

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
-const app = require('express')();
+const express = require('express');
 const PORT = 8080;
+const app = express();
+
+app.use(express.json());
+
+app.get('/tshirt', (req, res) => {
+	res.status(200).send({
+		tshirt: 'tshirticon',
+		size: 'large'
+	})
+});
+
 
 app.listen(
 	PORT,
 	() => console.log(`it's alive on http://localhost:${PORT}`));
 	
+app.post('/tshirt/:id', (req, res) => {
+	const { id } = req.params;
+	const { logo } = req.body;
+
+	if (!logo) {
+		res.status(418).send({ message: 'we need a logo!' })
+	}
+
+	res.send({
+		tshirt: `tshirticon with your ${logo} and ID of ${id}`,
+	});
+});
